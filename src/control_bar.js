629a144339d431b9d0774299a5e56576f6340950
BinaryMuse <brandon@brandontilley.com>
2015-02-24 15:39:00 -0800

    Pass props to containing span in ControlBar

--- src/control_bar.js
+++ src/control_bar.js
@@ -1,15 +1,14 @@
 var React = require("react"),
     el = React.createElement.bind(React);
 
-var KinetophoneEventMixin = require("./kinetophone_event_mixin"),
-    PlayPauseButton = require("./play_pause_button"),
+var PlayPauseButton = require("./play_pause_button"),
     SeekBar = require("./seek_bar");
 
 module.exports = React.createClass({
   displayName: "ReactKinetophoneControlBar",
 
   render: function() {
-    return React.DOM.span({},
+    return React.DOM.span(this.props,
       el(PlayPauseButton, {kinetophone: this.props.kinetophone}),
       el(SeekBar, {kinetophone: this.props.kinetophone})
     );
